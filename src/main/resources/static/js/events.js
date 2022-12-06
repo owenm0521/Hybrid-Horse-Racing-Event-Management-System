be9dfc0e28cb0bc91c6351eb4706264da3bff3f9
owenm0521 <owenm0521@gmail.com>
2022-12-06 18:13:05 -0500

    Added functionality to home page registration button. Updated layout and sizing of home page event list to accommodate longer event descriptions.

--- src/main/resources/static/js/events.js
+++ src/main/resources/static/js/events.js
@@ -19,37 +19,4 @@ $('document').ready(function(){
 		$('#deleteEventModal').modal();
 	});
 	
-	$('.table #registerButton').on('click',function(event){	
-		event.preventDefault();
-		var href = $(this).attr('href');
-		$.get(href, function(event){
-			$(".modal-header").html("Sign Up For " + event.name + ":");
-			$('#eventRegistrationModal #eventId').val(event.id); 
-		});
-		$('#eventRegistrationModal').modal();				
-	});	
-	
-	$('#eventRegistrationModal').on('submit',function(event){	
-		event.preventDefault(); 
-		var regSuccess = false; 
-		$.ajax({
-			type:"PUT",
-			url:"/events/registerForEvent",
-			data: $("#eventRegistrationForm").serialize(),
-			success: function(){
-				$('#eventRegistrationModal').modal('hide');
-				regSuccess = true; 
-			},
-			error: function(){
-				alert('Error processing registration. Pleasy try again.');
-			}
-		}); 
-		$(this).on('hidden.bs.modal',function(){
-			if(regSuccess){
-				alert('Your registration has been processed successfully!');
-				regSuccess = false; 
-			} 
-		}); 
-	});	 
-	
 });
